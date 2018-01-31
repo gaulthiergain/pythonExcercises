b1ccf5db259aa1910aff1a6f9f702e4652a7bab9
Gaulthier Gain <gaulthier.gain@student.ulg.ac.be>
2018-01-31 07:49:06 +0100

    Change print display

--- Exercise1.py
+++ Exercise1.py
@@ -25,29 +25,30 @@ def checkMask(mask):
 
 #1. Check the validity of ip address
 while True:
-	ip_address = raw_input("Enter Ip address: ")
+	ip_address = raw_input('Enter Ip address: ')
 	try:
 		# Use ipaddress module to check the validity of ip address
-		ip_address = unicode(ip_address, "UTF-8")
+		ip_address = unicode(ip_address, 'UTF-8')
 		ip_address = ipaddress.ip_address(ip_address)
 		break
 	except ValueError:
-		print("Invalid IP address format")
+		print 'Invalid IP address format'
 	
 #2. Check the validity of ip address
 while True:
-	ip_mask = raw_input("Enter subnet mask in decimal format: ")
+	ip_mask = raw_input('Enter subnet mask in decimal format: ')
 	try:
 		if(not checkMask(ip_mask)):
 			# Raise exception if the mask is invalid
 			raise Exception('Invalid Mask Exception')
 		break
 	except:
-			print("Subnet mask is invalid")
+			print 'Subnet mask is invalid'
 
 #3. Print ip address as binary format, network address and broadcast address
-print('{:8}'.format(' ') .join(x for x in str(ip_address).split('.')))
-print(' ' .join(format(int(x), '08b') for x in str(ip_address).split('.')))
+print '{:8}'.format(' ') .join(x for x in str(ip_address).split('.'))
+print ' ' .join(format(int(x), '08b') for x in str(ip_address).split('.'))
+# Create a IPv4Network object
 ip_network = ipaddress.ip_network(unicode(str(ip_address) + ip_mask), strict=False)
-print ("network address is: "+ str(ip_network))
-print("broadcast address is: " + str(ip_network.broadcast_address) + ip_mask)
+print 'network address is: ' + str(ip_network)
+print 'broadcast address is: ' + str(ip_network.broadcast_address) + ip_mask
